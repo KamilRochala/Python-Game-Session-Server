f9f6f6b5d6f04d44d5778a7c9306c194246f73a3
Kamil Rochala <rochalakamil2@gmail.com>
2026-05-06 20:15:26 +0200

    adding comments and additional if

--- server/classes/weapon.py
+++ server/classes/weapon.py
@@ -13,7 +13,7 @@ class WeaponType(str, Enum):
 class Weapon(BaseModel, Item):
     damage: int # The damage of the weapon
     healing_capacity: int # This should only be for clerics, the higher the number, the higher the ammount of health will be restored
-    weapon_type: WeaponType
+    weapon_type: WeaponType # Enumerator to set whose weapon is it
 
     @field_validator
     @classmethod
@@ -26,6 +26,10 @@ class Weapon(BaseModel, Item):
     @classmethod
     def validate_healing_capacity(cls, v, info):
         weapon_type = info.data.get('weapon_type')
+
+        # If weapon is not assigned to cleric class, 
         if v > 0 and weapon_type == WeaponType.KNIGHT:
             raise ValueError('Knights cannot use healing weapons')
+        elif v < 0:
+            raise ValueError('healing stat should be either at 0 or positive if its clerics weapon')
         return v
